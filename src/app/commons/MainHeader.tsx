6a132f039856cdf0748fce4b73a555124e0fd5f4
Thai (Isaac) Nguyen <thai.nguyentran@pycogroup.com>
2018-11-28 00:21:44 +0700

    Add fonts library and MainHeader's styling

--- src/app/commons/MainHeader.tsx
+++ src/app/commons/MainHeader.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { View, TextInput } from 'react-native';
+import { Icon } from 'react-native-elements';
+
 import { MAIN_HEADER_STYLES, COLORS } from '../style-sheets';
 
 export default function Header(props: any) {
   return (
     <View style={ MAIN_HEADER_STYLES.container }>
       <View style={ MAIN_HEADER_STYLES.mainWrapper }>
+        <View style={ MAIN_HEADER_STYLES.actionIcon }>
+          <Icon
+            name='camera'
+            type='entypo'
+            size={ 24 }
+            color={ COLORS.WHITE } />
+        </View>
         <TextInput
           style={ MAIN_HEADER_STYLES.searchInput }
           placeholder="Search"
           placeholderTextColor={ COLORS.FB_SEARCH_INPUT_PLACEHOLDER } />
+        <View style={ MAIN_HEADER_STYLES.actionIcon }>
+          <Icon
+            name='facebook-messenger'
+            type='material-community'
+            size={ 24 }
+            color={ COLORS.WHITE } />
+        </View>
       </View>
     </View>
   );
--- src/app/style-sheets/MainHeader.style.ts
+++ src/app/style-sheets/MainHeader.style.ts
@@ -1,8 +1,8 @@
-import { StyleProp, TextStyle } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { COLORS } from './variables.constant';
 
-export const MAIN_HEADER_STYLES = {
+export const MAIN_HEADER_STYLES = StyleSheet.create({
   container: {
     flex: 1,
     height: 90,
@@ -10,21 +10,28 @@ export const MAIN_HEADER_STYLES = {
     backgroundColor: COLORS.FB_HEADER,
     paddingTop: 45,
     paddingBottom: 5
-  } as StyleProp<TextStyle>,
+  },
   mainWrapper: {
-    flex: 1
-  } as StyleProp<TextStyle>,
-  searchInput: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  actionIcon: {
+    flex: 1,
+    textAlign: 'center'
+  },
+  searchInput: {
+    flex: 9,
+    height: 40,
+    maxHeight: 40,
     backgroundColor: COLORS.FB_SEARCH_INPUT,
     color: COLORS.WHITE,
     borderColor: COLORS.FB_SEARCH_INPUT,
     borderWidth: 1,
     borderRadius: 20,
-    marginLeft: 10,
-    marginRight: 10,
     paddingLeft: 10,
     paddingRight: 10,
     textAlign: 'center'
-  } as StyleProp<TextStyle>
-}
+  }
+});
