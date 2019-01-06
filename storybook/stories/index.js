460ddb0083628d6e4f7ae6e8fce409b40ca1926f
Thai (Isaac) Nguyen <thai.nguyentran@pycogroup.com>
2019-01-06 16:55:57 +0700

    Add storybook's stories for CardContentImage component

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -8,6 +8,7 @@ import { linkTo } from '@storybook/addon-links';
 import Button from './Button';
 import CenterView from './CenterView';
 import Welcome from './Welcome';
+import { CardContentImage } from '../../src/app/components';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -23,3 +24,34 @@ storiesOf('Button', module)
       <Text>😀 😎 👍 💯</Text>
     </Button>
   ));
+
+storiesOf('CardContentImage', module)
+  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+  .add('with 1 image content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  })
+  .add('with 2 images content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  })
+  .add('with 3 images content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  })
+  .add('with 4 images content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  })
+  .add('with 5 images content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  })
+  .add('with 6 images content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  })
+  .add('with more than 6 images content', () => {
+    const images = [require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg'), require('../../src/assets/27573875_363298654140769_4881900956687532032_n.jpg')];
+    return (<CardContentImage images={images} />);
+  });
